e3b967e41079b224951eff3e07e41563bc13d53b
Antonia <73851646+vanillepie@users.noreply.github.com>
2021-05-24 02:05:32 +0200

    Unnecessary imports removed

--- src/main/java/Utility/AuxiliaryWorker.java
+++ src/main/java/Utility/AuxiliaryWorker.java
@@ -1,17 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Utility;
 
-import java.util.Random;
-import java.util.concurrent.CountDownLatch;
-
-/**
- *
- * @author guial
- */
 public abstract class AuxiliaryWorker extends Thread{
     private final String auxWorkerID;
 
@@ -30,4 +18,3 @@ public abstract class AuxiliaryWorker extends Thread{
 
             
     }
-
